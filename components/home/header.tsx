1faa34f4bc693c104b28f39e3c9956f185533f99
Joel AMBU <ambujoel@gmail.com>
2025-02-20 12:56:31 +0100

    banner.

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='h-44 relative -mt-20' style={{ backgroundImage: "url('/tshilolo3.png')", backgroundSize: 'cover', backgroundPositionX: '20px'  }}>
+    <div className='h-44 relative -mt-20' style={{ backgroundImage: "url('/tshilolo3.png')", backgroundSize: 'cover', backgroundPositionX: '20px',backgroundPositionY: '-20px'  }}>
       <Image src='/fdlogo.svg' alt='banner'  height={150} width={120} className='absolute mt-10 ml-6'/>
     </div>
   )
